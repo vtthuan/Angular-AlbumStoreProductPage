bf254233f835a2213f58c38b0a0861dfad2a358b
vtthuan <vtthuan@hotmail.com>
2020-05-01 12:00:02 +0200

    part 3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { IProduct } from './iproduct';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  _albumUrl = "../assets/album.json";
+
+  constructor(private http : HttpClient) { }
+
+  getAlbum(id : number) {
+
+    return this.http.get(this._albumUrl);
+  }
 }
